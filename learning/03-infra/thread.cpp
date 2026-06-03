f9023025b3a5bd5c25e01072cfb9362dd211745f
Kwan <lightspots@hotmail.com>
2026-06-04 01:32:55 +0800

    feat(infra): patch ref to temp args in lambda body

--- learning/03-infra/thread.cpp
+++ learning/03-infra/thread.cpp
@@ -3,7 +3,7 @@
 int main() 
 {
     auto t = create_and_start_thread(1, "dbg", 
-        [](const std::string& arg) {std::println("hello {}", arg);}, "there");
+        [](const std::string& arg) {std::println("hello {}", arg);}, std::string("there"));
     std::this_thread::sleep_for(1s);
     t->join();
     delete t;
--- learning/03-infra/thread.hpp
+++ learning/03-infra/thread.hpp
@@ -7,17 +7,20 @@
 #include <chrono>
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <iostream>
 
 using namespace std::chrono_literals;
 
 inline auto set_thread_core([[maybe_unused]] int core_id) -> bool
 {
 #ifdef __linux__
+    std::println("setting thread affinity {}", core_id);
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
     CPU_SET(core_id, &cpuset);
     return (pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset) == 0);
 #else
+    std::println("skipping thread affinity for mac");
     return true;
 #endif
 }
@@ -25,10 +28,12 @@ inline auto set_thread_core([[maybe_unused]] int core_id) -> bool
 template<typename F, typename... A>
 inline auto create_and_start_thread(int core_id, const std::string& name, F&& func, A&&... args) -> std::thread*
 {
-    std::atomic<bool> running {false};
-    auto body = [&]() {
-        std::println("core {} thread {}", core_id, name);
-        running = true;
+    auto body = [core_id, name, func=std::forward<F>(func), ...args=std::forward<A>(args)]() mutable {
+        if (core_id >= 0 && !set_thread_core(core_id)) {
+            std::println("failed to set core affinity of thread {} {} to core {}", name, pthread_self(), core_id);
+            exit(EXIT_FAILURE);
+        }
+        std::println("core affinity of thread {} {} has been set to core {}", name, pthread_self(), core_id);
         std::forward<F>(func)(std::forward<A>(args)...);
     };
     auto thread = new std::thread(body);
