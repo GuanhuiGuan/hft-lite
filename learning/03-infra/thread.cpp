2beb42d05f09133947865ed1035d04e6dfe77abc
Kwan <lightspots@hotmail.com>
2026-06-04 01:46:34 +0800

    feat(infra): upgrade thread example

--- learning/03-infra/thread.cpp
+++ learning/03-infra/thread.cpp
@@ -1,12 +1,23 @@
 #include "thread.hpp"
 
+auto sleep_and_say(const std::string& msg, bool sleep) -> void
+{
+    std::println("starting ...");
+    if (sleep) {
+        std::this_thread::sleep_for(1s);
+    }
+    std::println("saying: {}", msg);
+    std::println("ending ...");
+}
+
 int main() 
 {
-    auto t = create_and_start_thread(1, "dbg", 
-        [](const std::string& arg) {std::println("hello {}", arg);}, std::string("there"));
-    std::this_thread::sleep_for(1s);
-    t->join();
-    delete t;
+    auto t1 = create_and_start_thread(1, "dbg", &sleep_and_say, std::string("there"), true);
+    auto t2 = create_and_start_thread(2, "dbg", &sleep_and_say, std::string("hello"), false);
+    t1->join();
+    t2->join();
+    delete t1;
+    delete t2;
 
     return 0;
 }
